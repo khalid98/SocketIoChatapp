d0fc595993e6d8f444a6b1d0abb4bec128f52cd7
khaled Adel Mohamed <33879750+khalid98@users.noreply.github.com>
2020-08-04 00:44:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const socketio = require('socket.io')(http)
 app.get('/', (req, res) => {
     res.send("Good Server")
 })
-var numUsers = 0;
 
 socketio.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
@@ -20,33 +19,7 @@ socketio.on("connection", (userSocket) => {
         userSocket.broadcast.emit("stop_typing", data)
     })
 })
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
 
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
-});
-socket.on('add user', (username) => {
-    if (addedUser) return;
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
 
 http.listen(process.env.PORT)
